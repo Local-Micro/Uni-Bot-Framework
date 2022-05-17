efa0a4ef29a9c56a635f4d14bc431eeb0f305bfa
Local-Micro <Local-Micro@outlook.com>
2022-05-18 07:02:01 +0800

    添加基本协程池功能

---
+++ pkg/core/goroutinepool.go
@@ -0,0 +1,33 @@
+package core
+
+type Goroutine struct {
+	function func()
+	stderr   chan error
+	stdout   chan string
+	signal   chan string
+}
+
+type GoroutinePool struct {
+	pool map[string]Goroutine
+}
+
+func NewGoroutinePool() (*GoroutinePool, error) {
+	return &GoroutinePool{
+		pool: make(map[string]Goroutine),
+	}, nil
+}
+
+func (g *GouroutinePool) NewGoruntinue(name string, function func()) error {
+	g.pool[name] = Gouroutine{
+		function: function,
+		stderr:   make(chan error, 1024),
+		stdout:   make(chan string, 1024),
+		signal:   make(chan string, 1024),
+	}
+	return nil
+}
+
+func (g *GoroutinePool) Start(name string) error {
+	go g.pool[name].function(g.pool[name].stderr, g.pool[name].stdout, g.pool[name].signal)
+	return nil
+}
