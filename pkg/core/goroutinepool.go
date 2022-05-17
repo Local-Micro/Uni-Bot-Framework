7d529919b8b4f01e133ee49f45510932ded21082
Local-Micro <Local-Micro@outlook.com>
2022-05-18 07:32:35 +0800

    完善协程池功能

--- pkg/core/goroutinepool.go
+++ pkg/core/goroutinepool.go
@@ -2,7 +2,7 @@ package core
 
 type Goroutine struct {
 	function func()
-	stderr   chan error
+	stderr   chan map[string]error
 	stdout   chan string
 	signal   chan string
 }
@@ -20,7 +20,7 @@ func NewGoroutinePool() (*GoroutinePool, error) {
 func (g *GouroutinePool) NewGoruntinue(name string, function func()) error {
 	g.pool[name] = Gouroutine{
 		function: function,
-		stderr:   make(chan error, 1024),
+		stderr:   make(chan map[string]error, 1024),
 		stdout:   make(chan string, 1024),
 		signal:   make(chan string, 1024),
 	}
@@ -29,5 +29,13 @@ func (g *GouroutinePool) NewGoruntinue(name string, function func()) error {
 
 func (g *GoroutinePool) Start(name string) error {
 	go g.pool[name].function(g.pool[name].stderr, g.pool[name].stdout, g.pool[name].signal)
+	return nil
+}
+
+func (g *GoroutinePool) Stop(name string) error {
+	g.pool[text].signal <- "stop"
+	if g.pool.stderr["stop"] != nil {
+		return g.pool.stderr["stop"]
+	}
 	return nil
 }
