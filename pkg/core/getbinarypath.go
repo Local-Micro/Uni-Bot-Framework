9b2d8b613e4adbbb24dd2df2851076f13b7ce748
Local-Micro <Local-Micro@outlook.com>
2022-05-18 17:25:46 +0800

    给getbinarydictory.go改个名字

---
+++ pkg/core/getbinarypath.go
@@ -0,0 +1,59 @@
+package core
+
+import (
+	"os"
+	_path "path"
+	"path/filepath"
+	"runtime"
+	"strings"
+)
+
+func GetBinaryPath() (string, error) {
+	tmp, err := getTmpDir()
+	if err != nil {
+		return "", err
+	}
+	path, err := byExecutable()
+	if err != nil {
+		return "", err
+	}
+	if strings.Contains(path, tmp) {
+		path, err = byCaller()
+		if err != nil {
+			return "", err
+		}
+	}
+	return path, nil
+}
+
+func byCaller() (string, error) {
+	var path string
+	_, name, _, ok := runtime.Caller(0)
+	if ok {
+		path = _path.Dir(name)
+	}
+	return path, nil
+}
+
+func byExecutable() (string, error) {
+	path, err := os.Executable()
+	if err != nil {
+		return "", err
+	}
+	path, err = filepath.EvalSymlinks(filepath.Dir(path))
+	if err != nil {
+		return "", err
+	}
+	return path, nil
+}
+
+func getTmpDir() (string, error) {
+	dir := os.Getenv("TMP")
+	if dir == "" {
+		dir = os.Getenv("TEMP")
+		if dir == "" {
+			dir = os.Getenv("TMPDIR")
+		}
+	}
+	return dir, nil
+}
